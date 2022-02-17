882edadec0f1d0a2ad781f356596d97410b44052
michee <micheebakanda@gmail.com>
2022-02-17 11:28:09 +0200

    live frame updated

--- pages/live.js
+++ pages/live.js
@@ -19,10 +19,56 @@ const live = () => {
     scrollTop();
   }, []);
 
+  const liveHour = new Date().getHours();
+  const isWednesday = (new Date().getDay() == 3);
+  const isThursday = (new Date().getDay() == 4);
+  const isLiveDay = (isWednesday || isThursday)
+  const isOnePM = (liveHour == 15)
+
+
+  
+
+
+ 
+
   return (
     <LivePageContainer>
       <IframeWrapper>
+
+        
+
+          {
+            ( isLiveDay && isOnePM ) ? <iframe
+            style={{
+              height: "100%",
+              width: "100%",
+              background: "url(./logo.png)",
+              backgroundSize: 120,
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center",
+            }}
+             scrolling="no" id="hearthis_at_track_6554137" width="100%" height="150" src="https://hearthis.at/activefm/live" frameborder="0" allowtransparency allow="autoplay"><p>Listen to <a href="ACTIVE FM" target="_blank">no name</a> <span>by</span><a href="ACTIVE FM" target="_blank" >ACTIVE FM</a> <span>on</span> <a href="Listen to DJ Sets, Mixes, Tracks and Sounds |  hearthis.at" target="_blank">hearthis.at</a></p></iframe> :
+
+            
         <iframe  style={{
+          height: "100%",
+          width: "100%",
+          background: "url(./logo.png)",
+          backgroundSize: 120,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+        }}
+      src="https://www.youtube.com/embed/rjDXZO-Ek24" 
+      title="YouTube video player" 
+      frameborder="0" 
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+      allowfullscreen
+      />
+          }
+    
+   
+        
+        {/* <iframe  style={{
                     height: "100%",
                     width: "100%",
                     background: "url(./logo.png)",
@@ -35,7 +81,7 @@ const live = () => {
                 frameborder="0" 
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
                 allowfullscreen
-                />
+                /> */}
       </IframeWrapper>
     </LivePageContainer>
   );
