68f6ffc4d1da7cde734a10dd29f38a424b350786
4p0ca1yp53 <12derciomaduna@gmail.com>
2022-02-17 15:32:39 +0200

    Fixed the dynamic iFrame

--- pages/live.js
+++ pages/live.js
@@ -19,57 +19,54 @@ const live = () => {
     scrollTop();
   }, []);
 
-  let liveIframe = "https://hearthis.at/activefm/live"
+  let liveIframe = "https://hearthis.at/activefm/live";
   const liveHour = new Date().getHours();
   const isWednesday = new Date().getDay() == 3;
   const isThursday = new Date().getDay() == 4;
   const isLiveDay = isWednesday || isThursday;
   const isOnePM = liveHour == 15;
 
-  // console.log("Heart Radio")
-  // console.log(new Date().getHours())
-  // console.log(isOnePM , isLiveDay , liveIframe)
   return (
     <LivePageContainer>
       <IframeWrapper>
-        {/* {isLiveDay && isOnePM ? ( */}
-          <iframe
-            style={{
-              height: "100%",
-              width: "100%",
-              background: "url(./logo.png)",
-              backgroundSize: 120,
-              backgroundRepeat: "no-repeat",
-              backgroundPosition: "center",
-            }}
-            scrolling="no"
-            id="hearthis_at_track_6554137"
-            width="100%"
-            height="150"
-            src={liveIframe}
-            frameBorder="0"
-            allow="autoplay"
+        {isLiveDay && isOnePM ? (
+        <iframe
+          style={{
+            height: "100%",
+            width: "100%",
+            background: "url(./logo.png)",
+            backgroundSize: 120,
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+          }}
+          scrolling="no"
+          id="hearthis_at_track_6554137"
+          width="100%"
+          height="150"
+          src={liveIframe}
+          frameBorder="0"
+          allow="autoplay"
+        >
+          <p>
+            Listen to{" "}
+            <a href="ACTIVE FM" target="_blank">
+              no name
+            </a>{" "}
+            <span>by</span>
+            <a href="ACTIVE FM" target="_blank">
+              ACTIVE FM
+            </a>{" "}
+            <span>on</span>{" "}
+            <a
+              href="Listen to DJ Sets, Mixes, Tracks and Sounds |  hearthis.at"
+              target="_blank"
             >
-            <p>
-              Listen to{" "}
-              <a href="ACTIVE FM" target="_blank">
-                no name
-              </a>{" "}
-              <span>by</span>
-              <a href="ACTIVE FM" target="_blank">
-                ACTIVE FM
-              </a>{" "}
-              <span>on</span>{" "}
-              <a
-                href="Listen to DJ Sets, Mixes, Tracks and Sounds |  hearthis.at"
-                target="_blank"
-                >
-                hearthis.at
-              </a>
-            </p>
-          </iframe>
-        ) 
-        {/* : (
+              hearthis.at
+            </a>
+          </p>
+        </iframe>
+        )
+        : (
           <iframe
             style={{
               height: "100%",
@@ -85,9 +82,8 @@ const live = () => {
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen
           />
-        )} */}
-
-        {/* <iframe  style={{
+        )}   
+              {/* <iframe  style={{
                     height: "100%",
                     width: "100%",
                     background: "url(./logo.png)",
